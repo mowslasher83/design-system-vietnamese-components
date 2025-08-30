5873d1556d88298717c761f8ae2107bb1f57f7d3
mowslasher83 <radeswaled@hotmail.com>
2025-08-30 01:25:23 -0500

    Add comments to explain webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  // Entry point for the application
   entry: './src/index.js',
   output: {
+    // Output directory and filename
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/'
@@ -10,22 +12,26 @@ module.exports = {
   module: {
     rules: [
       {
+        // Rule for processing JavaScript and JSX files
         test: /\.jsx?$/,
         exclude: /node_modules/, 
         use: {
           loader: 'babel-loader',
           options: {
+            // Babel presets for transpilation
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
     ]
   },
+  // Dev server configuration
   devServer: {
     contentBase: './dist',
     hot: true
   },
   resolve: {
+    // File extensions that should be resolved
     extensions: ['.js', '.jsx']
   }
 };
